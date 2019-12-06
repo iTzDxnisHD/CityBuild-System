734c5ca565a065e1549a70cd13c8566e1ab8b675
iTzDxnisHD <57457977+iTzDxnisHD@users.noreply.github.com>
2019-12-06 14:59:48 +0100

    Create HealCommand.php

---
+++ src/CB/Commands/HealCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CB\Commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use CB\main;
+
+class HealCommand extends VanillaCommand {
+    public $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+        parent::__construct("heal", "Setze dein Leben auf voll", "/heal");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if ($sender instanceof Player) {
+            if ($sender->hasPermission("cb.heal")) {
+                $sender->setHealth(20);
+                $sender->sendMessage($this->plugin->prefix. "Du hast dich Gehealth§7!");
+                return true;
+        } else {
+            $sender->sendMessage($this->plugin->prefix. "§cDu hast nicht die nötigen Berechtigung, um diesen Command Auszuführen§7!");
+            return true;
+        }
+        }
+    }
+}
