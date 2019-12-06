4c4ac04a46b98a31d04f09bcf059489c27824ca1
NxtUserHD <50436997+NxtUser-HD@users.noreply.github.com>
2019-12-06 16:46:12 +0100

    oops

--- src/CB/Commands/SpawnCommand.php
+++ src/CB/Commands/SpawnCommand.php
@@ -10,11 +10,13 @@ use pocketmine\level\Level;
 use CB\main;
 use pocketmine\utils\Config;
 
-class SpawnCommand extends VanillaCommand {
+class SpawnCommand extends VanillaCommand
+{
 
     public $plugin;
 
-    public function __construct(Main $plugin) {
+    public function __construct(Main $plugin)
+    {
         $this->plugin = $plugin;
         parent::__construct("spawn", "Teleportiere dich zum Spawn", "/spawn");
     }
@@ -22,14 +24,12 @@ class SpawnCommand extends VanillaCommand {
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
         if ($sender instanceof Player) {
-            if ($this->plugin->config instanceof Config) {
-                if ($sender->hasPermission("cb.spawn")) {
-                    $sender->teleport($this->plugin->getServer()->getDefaultLevel()->getSafeSpawn());
-                    return true;
-                } else {
-                    $sender->sendMessage($this->plugin->prefix . "§cDu hast nicht die nötigen Berechtigung, um diesen Command Auszuführen§7!");
-                    return true;
-                }
+            if ($sender->hasPermission("cb.spawn")) {
+                $sender->teleport($this->plugin->getServer()->getDefaultLevel()->getSafeSpawn());
+                return true;
+            } else {
+                $sender->sendMessage($this->plugin->prefix . "§cDu hast nicht die nötigen Berechtigung, um diesen Command Auszuführen§7!");
+                return true;
             }
         }
         return true;
